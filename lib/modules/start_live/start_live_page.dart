8d3b9645fdfc929a4a0462c647902694e90ea67a
zainibrahem <zainibrahem46@gmail.com>
2023-11-02 10:49:14 -0500

    Fixing the view of the start live screen

--- lib/modules/start_live/start_live_page.dart
+++ lib/modules/start_live/start_live_page.dart
@@ -102,140 +102,140 @@ class _StartLivePageState extends State<StartLivePage>
                 return Stack(
                   alignment: Alignment.bottomCenter,
                   children: [
-                    // Visibility(
-                    //   visible:
-                    //       !keyboardVisible && !animationController.isCompleted,
-                    //   child: Container(
-                    //     alignment: Alignment.bottomRight,
-                    //     width: double.infinity,
-                    //     child: ShaderMask(
-                    //       shaderCallback: (Rect rect) {
-                    //         return LinearGradient(
-                    //           begin: Alignment.topCenter,
-                    //           end: Alignment.bottomCenter,
-                    //           colors: [
-                    //             mainBlueColor,
-                    //             mainBlueColor,
-                    //             Colors.transparent,
-                    //             Colors.transparent
-                    //           ],
-                    //           stops: const [0.0, 0.1, 0.9, 1.0],
-                    //         ).createShader(rect);
-                    //       },
-                    //       blendMode: BlendMode.dstOut,
-                    //       child: Lottie.asset(
-                    //         'assets/lottie/hearts-feedback.json',
-                    //         height: 200.h,
-                    //         fit: BoxFit.contain,
-                    //         repeat: false,
-                    //         controller: animationController,
-                    //       ),
-                    //     ),
-                    //   ),
-                    // ),
-                    // CommentsLiveList(
-                    //   height: keyboardVisible
-                    //       ? 0.15.sh
-                    //       : startLiveCubit.heightComments,
-                    // ),
-                    // Visibility(
-                    //   visible: !keyboardVisible,
-                    //   child: Container(
-                    //     alignment: Alignment.bottomRight,
-                    //     height: 40.h,
-                    //     child: IconButton(
-                    //       onPressed: () =>
-                    //           startLiveCubit.onShowDescription(context),
-                    //       icon: const Icon(
-                    //         Icons.keyboard_arrow_up_outlined,
-                    //         color: Colors.white,
-                    //       ),
-                    //     ),
-                    //   ),
-                    // ),
+                    Visibility(
+                      visible:
+                          !keyboardVisible && !animationController.isCompleted,
+                      child: Container(
+                        alignment: Alignment.bottomRight,
+                        width: double.infinity,
+                        child: ShaderMask(
+                          shaderCallback: (Rect rect) {
+                            return LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                mainBlueColor,
+                                mainBlueColor,
+                                Colors.transparent,
+                                Colors.transparent
+                              ],
+                              stops: const [0.0, 0.1, 0.9, 1.0],
+                            ).createShader(rect);
+                          },
+                          blendMode: BlendMode.dstOut,
+                          child: Lottie.asset(
+                            'assets/lottie/hearts-feedback.json',
+                            height: 200.h,
+                            fit: BoxFit.contain,
+                            repeat: false,
+                            controller: animationController,
+                          ),
+                        ),
+                      ),
+                    ),
+                    CommentsLiveList(
+                      height: keyboardVisible
+                          ? 0.15.sh
+                          : startLiveCubit.heightComments,
+                    ),
+                    Visibility(
+                      visible: !keyboardVisible,
+                      child: Container(
+                        alignment: Alignment.bottomRight,
+                        height: 40.h,
+                        child: IconButton(
+                          onPressed: () =>
+                              startLiveCubit.onShowDescription(context),
+                          icon: const Icon(
+                            Icons.keyboard_arrow_up_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 );
               },
             ),
-            // Row(
-            //   children: [
-            //     Expanded(
-            //       child: Padding(
-            //         padding: EdgeInsets.symmetric(
-            //           horizontal: 10.w,
-            //         ),
-            //         child: BlocConsumer<StartLiveCubit, StartLiveState>(
-            //           listener: (context, state) {},
-            //           builder: (context, state) {
-            //             return LiveMessageInputField(
-            //               suffixIcon: IconButton(
-            //                   padding: EdgeInsets.zero,
-            //                   style: const ButtonStyle(
-            //                       tapTargetSize:
-            //                           MaterialTapTargetSize.shrinkWrap),
-            //                   onPressed: () => keyboardVisible
-            //                       ? startLiveCubit.sendComment()
-            //                       : startLiveCubit.emojiPicker(context),
-            //                   icon: SvgPicture.asset(
-            //                     color: newLightGreyColor,
-            //                     keyboardVisible
-            //                         ? 'assets/svg/icons/shareLocation.svg'
-            //                         : 'assets/svg/vmoji_outline.svg',
-            //                   )),
-            //               controller: startLiveCubit.liveMessageText,
-            //             );
-            //           },
-            //         ),
-            //       ),
-            //     ),
-            //     Visibility(
-            //       visible: !keyboardVisible,
-            //       child: IconButton(
-            //         onPressed: () {},
-            //         icon: SvgPicture.asset(
-            //           'assets/svg/camera_outline_gray.svg',
-            //         ),
-            //       ),
-            //     ),
-            //     Visibility(
-            //       visible: !keyboardVisible,
-            //       child: IconButton(
-            //         onPressed: () {},
-            //         icon: SvgPicture.asset(
-            //           'assets/svg/user_add_gray.svg',
-            //         ),
-            //       ),
-            //     ),
-            //     Visibility(
-            //       visible: !keyboardVisible,
-            //       child: LikeButton(
-            //         size: 25.sp,
-            //         padding: EdgeInsets.symmetric(horizontal: 5.w),
-            //         onTap: (isLiked) async {
-            //           isLiked ? null : animationController.reset();
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10.w,
+                    ),
+                    child: BlocConsumer<StartLiveCubit, StartLiveState>(
+                      listener: (context, state) {},
+                      builder: (context, state) {
+                        return LiveMessageInputField(
+                          suffixIcon: IconButton(
+                              padding: EdgeInsets.zero,
+                              style: const ButtonStyle(
+                                  tapTargetSize:
+                                      MaterialTapTargetSize.shrinkWrap),
+                              onPressed: () => keyboardVisible
+                                  ? startLiveCubit.sendComment()
+                                  : startLiveCubit.emojiPicker(context),
+                              icon: SvgPicture.asset(
+                                color: newLightGreyColor,
+                                keyboardVisible
+                                    ? 'assets/svg/icons/shareLocation.svg'
+                                    : 'assets/svg/vmoji_outline.svg',
+                              )),
+                          controller: startLiveCubit.liveMessageText,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Visibility(
+                  visible: !keyboardVisible,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      'assets/svg/camera_outline_gray.svg',
+                    ),
+                  ),
+                ),
+                Visibility(
+                  visible: !keyboardVisible,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      'assets/svg/user_add_gray.svg',
+                    ),
+                  ),
+                ),
+                Visibility(
+                  visible: !keyboardVisible,
+                  child: LikeButton(
+                    size: 25.sp,
+                    padding: EdgeInsets.symmetric(horizontal: 5.w),
+                    onTap: (isLiked) async {
+                      isLiked ? null : animationController.reset();
 
-            //           animationController.duration =
-            //               const Duration(milliseconds: 1500);
+                      animationController.duration =
+                          const Duration(milliseconds: 1500);
 
-            //           isLiked ? null : animationController.forward();
+                      isLiked ? null : animationController.forward();
 
-            //           startLiveCubit.onLikeButtonTapped(isLiked);
+                      startLiveCubit.onLikeButtonTapped(isLiked);
 
-            //           return !isLiked;
-            //         },
-            //         likeBuilder: (bool isLiked) {
-            //           return SvgPicture.asset(
-            //             isLiked
-            //                 ? 'assets/svg/icons/love heart.svg'
-            //                 : 'assets/svg/icons/loveHeart.svg',
-            //             color: isLiked ? alertRedColor : newLightGreyColor,
-            //           );
-            //         },
-            //       ),
-            //     ),
-            //   ],
-            // ),
-            // heightBox(10.h)
+                      return !isLiked;
+                    },
+                    likeBuilder: (bool isLiked) {
+                      return SvgPicture.asset(
+                        isLiked
+                            ? 'assets/svg/icons/love heart.svg'
+                            : 'assets/svg/icons/loveHeart.svg',
+                        color: isLiked ? alertRedColor : newLightGreyColor,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+            heightBox(10.h)
           ]),
         ),
       ),

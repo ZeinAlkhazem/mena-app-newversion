1064023a5b7a78656afff2eb5236f17ec2bc7f1f
zainibrahem <zainibrahem46@gmail.com>
2023-11-13 09:33:23 -0600

    Adding Waddah Code to Main

--- lib/MenaMarketPlace/features/market/presentation/widgets/search_controll.dart
+++ lib/MenaMarketPlace/features/market/presentation/widgets/search_controll.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mena/core/functions/main_funcs.dart';
-
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import '../../../../../core/constants/Colors.dart';
 import 'search_box.dart';
 
@@ -15,14 +15,22 @@ class SearchControll extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        SvgPicture.asset(
-          "assets/menamarket/qr_code_outline_28.svg",color: AppColors.whiteCreamColor,
+        InkWell(
+          onTap: () async {
+            await FlutterBarcodeScanner.scanBarcode(
+                "#ff6666", "Cancel", false, ScanMode.QR);
+          },
+          child: SvgPicture.asset(
+            "assets/menamarket/qr_code_outline_28.svg",
+            color: AppColors.whiteCreamColor,
+          ),
         ),
         widthBox(8.w),
         Flexible(child: SearchBox()),
         widthBox(8.w),
          SvgPicture.asset(
-          "assets/menamarket/menu_28.svg",color: AppColors.whiteCreamColor,
+          "assets/menamarket/menu_28.svg",
+          color: AppColors.whiteCreamColor,
         ),
       ],
     );
